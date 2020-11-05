249a6ebf77baa43f083005481039d34e6f6aa184
Mace <maceplus@gmail.com>
2020-11-05 15:07:12 +0000

    test to prevent takeoff when stormy

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -20,4 +20,11 @@ describe('Feature Test:', function(){
     expect(airport.planes()).not.toContain(plane);
   });
 
+  it('blocks takeoff when weather is stormy', function(){
+    plane.land(airport)
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
+    expect(airport.planes()).toContain(plane);
+  });
+
 });
